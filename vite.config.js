d20fa23f898e3c8c177802b4dcaebc1b9d4de542
Ben Hohner <benhohner@gmail.com>
2023-04-03 14:40:00 -0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import globalStyle from '@originjs/vite-plugin-global-style'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), globalStyle()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
